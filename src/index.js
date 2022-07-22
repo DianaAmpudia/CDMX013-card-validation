cb064df0069d9fc7823b02ae2cb481a099e1aac8
DianaAmpudia <dianalauraampudia@gmail.com>
2022-07-22 15:40:17 -0500

    final

--- src/index.js
+++ src/index.js
@@ -9,26 +9,26 @@ const mInvalido2= document.getElementById("mensaje2")
 
 botonValidar.addEventListener("click",validarNumTarjeta);
 
-
 function validarNumTarjeta() {
 
-if (creditCardNumber.value.length <= 12) {
-  mInvalido.innerHTML="Por favor ingresa al menos 13 dígitos"
-  mValido2.innerHTML= ""
-
-  //Condicionales anidados
-
-} else if (validator.isValid(creditCardNumber.value)){
-     mValido.innerHTML="TU TARJETA FUE VALIDADA CON EXITO."
-     mValido2.innerHTML= "Puedes continuar con tu compra."
-
-     creditCardNumber.value= validator.maskify(creditCardNumber.value);
- }
-  else{
-    mInvalido.innerHTML="TU TARJETA NO ES VALIDA"
-    mInvalido2.innerHTML="Por favor ingresa otra tarjeta para poder continuar con tu compra."
-
-    creditCardNumber.value= validator.maskify(creditCardNumber.value);
+  if (creditCardNumber.value.length <= 12) {
+    mInvalido.innerHTML="Por favor ingresa al menos 13 dígitos"
+    mValido2.innerHTML= ""
+  
+    //Condicionales anidados
+  
+  } else if (validator.isValid(creditCardNumber.value)){
+       mValido.innerHTML="TU TARJETA FUE VALIDADA CON EXITO."
+       mValido2.innerHTML= "Puedes continuar con tu compra."
+  
+  
+       creditCardNumber.value= validator.maskify(creditCardNumber.value);
+   }
+    else{
+      mInvalido.innerHTML="TU TARJETA NO ES VALIDA"
+      mInvalido2.innerHTML="Por favor verifica tu datos o ingresa otra tarjeta para poder continuar con tu compra."
+  
+      creditCardNumber.value= validator.maskify(creditCardNumber.value);
+    }
+  
   }
-
-}
